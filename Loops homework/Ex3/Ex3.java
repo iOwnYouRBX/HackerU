01bc9693b7cf03408843275ce596b4e157ac099c
iOwn_You <33134643+iOwnYouRBX@users.noreply.github.com>
2022-11-20 15:04:56 +0200

    Add files via upload

---
+++ Loops homework/Ex3/Ex3.java
@@ -0,0 +1,18 @@
+/*
+1 -
+    Outputs 10 "*" vertically (each in a new line)
+
+2 -
+    a function that loops x (parameter) amount of times,
+    every 3 steps, including 0, it will increase the "c" variable by 1 - essentially "Math.floor(x / 3);"
+    in the end it returns the "c" variable
+
+3 -
+    a function that loops while x is bigger than 0 (x > 0)
+    each time it divides x by 10 and adding +1 to the "c" variable, essentially counting how many times x can divide by 10
+    after the loop ends it returns the "c" variable
+
+
+
+
+ */
